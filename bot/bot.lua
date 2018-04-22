5c74feb13233dc0a6d02fa2f8983a0ef94b1bae3
saman9074 <saman9074@yahoo.com>
2018-04-22 19:49:24 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -90,28 +90,16 @@ function create_config( )
 		"fun",
 
 	},
-    sudo_users = {111334847, 157059515, sudo_id},
+    sudo_users = {122630959, 55670780, sudo_id},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v6.0
-An advanced administration bot based on https://valtman.name/telegram-cli
+    info_text = [[》ArcaneTeam v1
 
-》https://github.com/BeyondTeam/BDReborn 
-
-》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@ToOfan ➣ Developer《
-
-》Special thanks to :
-》Beyond Team Members
-
-》Our channel :
-》@BeyondTeam《
+》https://github.com/saman9074/ 
 
 》Our website :
-》http://Beyond-Dev.iR
+》http://arcaneteam.iR
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
